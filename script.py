12e4fb0ce80e5b82dd70c627faec499124dcf4b8
blckvia <a.vyatkinn@icloud.com>
2023-11-06 13:18:08 +0300

    feat: additional option has been added

--- script.py
+++ script.py
@@ -17,16 +17,34 @@ def process_excel_file(file_path):
     data_xls = pd.read_excel(file_path, index_col=None)
 
     def extract_custom(value):
-        # match for 12 digits start from 1
-        match = re.search(r'\b1\d{11}\b', str(value))
-        # match for 11 digits start from 2
-        match_2 = re.search(r'\b2\d{11}\b', str(value))
-        # match = re.search(r'1\d{6}(?!(00010))\d{5}', str(value))
-        # match = re.search(r'1\d{11}', str(value))
+        match = re.search(r'(?:[ =]|en|ru)?1\d{11}\b', str(value))
+        match_2 = re.search(r'(?:[ =]|en|ru)2\d{11}\b', str(value))
+        match_3 = re.search(r'(?:[ =]|en|ru)?214210\d{4}\b', str(value))
+        match_4 = re.search(r'(?:[ =]|en|ru)?210\d{9}\b', str(value))
+        match_5 = re.search(r'(?:[ =]|en|ru)?214\d{9}\b', str(value))
+        match_6 = re.search(r'(?:[ =]|en|ru)?215\d{9}\b', str(value))
+        match_7 = re.search(r'(?:[ =]|en|ru)?220\d{9}\b', str(value))
+        match_8 = re.search(r'(?:[ =]|en|ru)?250\d{9}\b', str(value))
+        match_9 = re.search(r'(?:[ =]|en|ru)?260\d{9}\b', str(value))
+        match_9 = re.search(r'(?:[ =]|en|ru)?270\d{9}\b', str(value))
         if match:
             return match.group()
         elif match_2:
             return match_2.group()
+        elif match_3:
+            return match_3.group()
+        elif match_4:
+            return match_4.group()
+        elif match_5:
+            return match_5.group()
+        elif match_6:
+            return match_6.group()
+        elif match_7:
+            return match_7.group()
+        elif match_8:
+            return match_8.group()
+        elif match_9:
+            return match_9.group()
         else:
             return None
         # return match.group() if match else None
@@ -48,6 +66,10 @@ def process_excel_file(file_path):
     # Filter rows where "Unnamed: 20" contains 11 digits starting with "1" and not ending with "00001"
     data_xls = data_xls[data_xls["Unnamed: 20"].notna()]
 
+    # Clean the "Unnamed: 20" column to remove spaces and equal signs
+    data_xls["Unnamed: 20"] = data_xls["Unnamed: 20"].str.replace(
+        r'[ =]', '', regex=True)
+
     data_xls = data_xls[data_xls["Unnamed: 13"].notna()]
 
     data_xls = data_xls[data_xls["Unnamed: 1"].notna()]
@@ -58,6 +80,14 @@ def process_excel_file(file_path):
     # Save the result as a CSV with semicolon as the separator
     result_df.to_csv('your_csv.csv', encoding='utf-8',
                      index=False, sep=';', header=False)
+    # Read the CSV file
+    df = pd.read_csv('your_csv.csv', sep=';', header=None)
+
+    # Remove spaces and equal signs from the "Unnamed: 20" column
+    df[2] = df[2].str.replace('[ =]', '', regex=True)
+
+    # Save the cleaned data to a new CSV file
+    df.to_csv('cleaned_csv.csv', sep=';', index=False, header=False)
 
 
 # Use glob to find all Excel files (with .xlsx extension) in the folder
